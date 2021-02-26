abf9345d279813ab1ba91ded1bd3a421b9c723fd
Hongyi Liu <hl2638@nyu.edu>
2021-02-26 23:34:57 +0800

    Implemented PlayAudio, Kana

---
+++ code/Kana.py
@@ -0,0 +1,14 @@
+class Kana:
+    def __init__(self, roma, hira, kata):
+        self.hira = hira
+        self.kata = kata
+        self.roma = roma
+
+    def getHira(self):
+        return self.hira
+
+    def getKata(self):
+        return self.kata
+
+    def getRoma(self):
+        return self.roma
